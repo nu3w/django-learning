64095c82340850544ced60d02c7a8cbc93afc827
menu <lahkuhasunem@gmail.com>
2026-05-20 08:30:23 +0545

    Added update and delete features

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import recipe_list
-from .views import create
+from .views import recipe_list, create, update, delete
 
 urlpatterns = [
     path('recipe/', recipe_list),
-    path('recipe/create/', create)
+    path('recipe/create/', create),
+    path('recipe/<id>/', update),
+    path('delete/<int:id>/', delete, name="delete")
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -21,3 +21,26 @@ def create(request):
         Recipe.objects.create(name = name, ingredient = ingredient, instruction = instruction, time =  time, level = level)
         return redirect('/recipe/')
     return render(request, "create.html")
+
+def update(request, id):
+    recipe = Recipe.objects.get(id = id)
+    context = {"recipe" : recipe}
+    if request.method == "POST":
+        name = request.POST.get("name")
+        ingredient = request.POST.get("ingredient")
+        instruction = request.POST.get("instruction")
+        time = request.POST.get("time")
+        level = request.POST.get("level")
+        recipe.name = name
+        recipe.ingredient = ingredient
+        recipe.instruction = instruction
+        recipe.time = time
+        recipe.level = level
+        recipe.save()
+        return redirect('/recipe/')
+    return render(request, "update.html", context)
+
+def delete(request, id):
+    recipe = Recipe.objects.get(id = id)
+    recipe.delete()
+    return redirect('/recipe/')
